d2906abf9ee2eb129cf8e00ee245daf73326e122
Tomislav Milkovic <tomislav.milkovic95@gmail.com>
2022-03-09 11:29:52 +0100

    drivers: clock_control: clock_stm32_ll_h7: Fix PLL3 PQR output init
    Due to typo in macro name, PLL3P, PLL3Q and PLL3R outputs
    are never enabled
    
    Change-Id: I21de198b2c06a88e7117d0ee9b29ca63732c9f60
    Signed-off-by: Tomislav Milkovic <tomislav.milkovic95@gmail.com>

--- subsys/bluetooth/mesh/pb_gatt_srv.c
+++ subsys/bluetooth/mesh/pb_gatt_srv.c
@@ -302,7 +302,7 @@ int bt_mesh_pb_gatt_srv_adv_start(void)
 	return err;
 }
 
-BT_CONN_CB_DEFINE(pb_gatt_srv) = {
+BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.connected = gatt_connected,
 	.disconnected = gatt_disconnected,
 };
