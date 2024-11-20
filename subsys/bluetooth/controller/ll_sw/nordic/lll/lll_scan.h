zephyrproject-rtos/zephyr

Bluetooth: controller: split: SCAN_RSP for ADV_IND or SCAN_IND
Added implementation to differentiate SCAN_RSP received for
ADV_IND or SCAN_IND PDU.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan.h
@@ -24,6 +24,7 @@ struct lll_scan {
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
 	u8_t  phy:3;
+	u8_t  is_adv_ind:1;
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 #if defined(CONFIG_BT_CTLR_PRIVACY)

gzh-terry/external_zblue

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan.h
@@ -24,6 +24,7 @@ struct lll_scan {
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
 	uint8_t  phy:3;
+	uint8_t  is_adv_ind:1;
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 #if defined(CONFIG_BT_CTLR_PRIVACY)