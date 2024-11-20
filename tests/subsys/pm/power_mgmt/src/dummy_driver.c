yperess/zephyr

tests: pm: use new PM macros
Use PM_DEVICE_STATE_DEFINE to define PM state.

Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

tests/subsys/pm/power_mgmt/src/dummy_driver.c
@@ -36,6 +36,8 @@ int dummy_init(const struct device *dev)
 	return 0;
 }
 
+PM_DEVICE_DEFINE(dummy_driver, dummy_device_pm_ctrl);
+
 DEVICE_DEFINE(dummy_driver, DUMMY_DRIVER_NAME, &dummy_init,
-		    dummy_device_pm_ctrl, NULL, NULL, APPLICATION,
-		    CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &funcs);
+	      &PM_DEVICE_GET(dummy_driver), NULL, NULL, APPLICATION,
+	      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &funcs);

gzh-terry/external_zblue

tests/subsys/pm/power_mgmt/src/dummy_driver.c
@@ -36,6 +36,8 @@ int dummy_init(const struct device *dev)
 	return 0;
 }
 
+PM_DEVICE_DEFINE(dummy_driver, dummy_device_pm_action);
+
 DEVICE_DEFINE(dummy_driver, DUMMY_DRIVER_NAME, &dummy_init,
-		    dummy_device_pm_action, NULL, NULL, APPLICATION,
-		    CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &funcs);
+	      PM_DEVICE_REF(dummy_driver), NULL, NULL, APPLICATION,
+	      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &funcs);