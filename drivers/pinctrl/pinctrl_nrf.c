jgediya/zephyr

drivers: pinctrl: nrf: add support for uart/uarte peripheral
Add support for configuring UART/UARTE peripheral pins.

Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

drivers/pinctrl/pinctrl_nrf.c
@@ -8,6 +8,14 @@
 
 #include <hal/nrf_gpio.h>
 
+#if DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_uart)
+#define NRF_UART_TYPE NRF_UART_Type
+#define NRF_UART_PSEL(uart, line) uart->PSEL##line
+#elif DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_uarte)
+#define NRF_UART_TYPE NRF_UARTE_Type
+#define NRF_UART_PSEL(uart, line) uart->PSEL.line
+#endif
+
 /**
  * @brief Configure pin settings.
  *
@@ -28,10 +36,69 @@ static void nrf_pin_configure(pinctrl_soc_pin_t pin, nrf_gpio_pin_dir_t dir,
 		     NRF_GET_DRIVE(pin), NRF_GPIO_PIN_NOSENSE);
 }
 
+#if defined(NRF_UART_TYPE)
+/**
+ * @brief Configure pins for the UART/UARTE peripheral.
+ *
+ * @param uart UART/UARTE device.
+ * @param pins Pins to be configured.
+ * @param pin_cnt Number of pins.
+ */
+static void pinctrl_nrf_uart_config(NRF_UART_TYPE *uart,
+				    const pinctrl_soc_pin_t *pins,
+				    uint8_t pin_cnt)
+{
+	for (uint8_t i = 0U; i < pin_cnt; i++) {
+		switch (NRF_GET_FUN(pins[i])) {
+		case NRF_FUN_UART_TX:
+			NRF_UART_PSEL(uart, TXD) = NRF_GET_PIN(pins[i]);
+			nrf_gpio_pin_write(NRF_GET_PIN(pins[i]), 1);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_OUTPUT,
+					  NRF_GPIO_PIN_INPUT_DISCONNECT);
+			break;
+		case NRF_FUN_UART_RX:
+			NRF_UART_PSEL(uart, RXD) = NRF_GET_PIN(pins[i]);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_INPUT,
+					  NRF_GPIO_PIN_INPUT_CONNECT);
+			break;
+		case NRF_FUN_UART_RTS:
+			NRF_UART_PSEL(uart, RTS) = NRF_GET_PIN(pins[i]);
+			nrf_gpio_pin_write(NRF_GET_PIN(pins[i]), 1);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_OUTPUT,
+					  NRF_GPIO_PIN_INPUT_DISCONNECT);
+			break;
+		case NRF_FUN_UART_CTS:
+			NRF_UART_PSEL(uart, CTS) = NRF_GET_PIN(pins[i]);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_INPUT,
+					  NRF_GPIO_PIN_INPUT_CONNECT);
+			break;
+		default:
+			break;
+		}
+	}
+}
+#endif /* defined(NRF_UART_TYPE) */
+
 int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 			   uintptr_t reg)
 {
 	switch (reg) {
+#if defined(NRF_UART_TYPE)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart0), okay)
+	case DT_REG_ADDR(DT_NODELABEL(uart0)):
+#endif
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart1), okay)
+	case DT_REG_ADDR(DT_NODELABEL(uart1)):
+#endif
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart2), okay)
+	case DT_REG_ADDR(DT_NODELABEL(uart2)):
+#endif
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart3), okay)
+	case DT_REG_ADDR(DT_NODELABEL(uart3)):
+#endif
+		pinctrl_nrf_uart_config((NRF_UART_TYPE *)reg, pins, pin_cnt);
+		break;
+#endif /* defined(NRF_UART_TYPE) */
 	default:
 		return -ENOTSUP;
 	}

gzh-terry/external_zblue

Co-authored-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

drivers/pinctrl/pinctrl_nrf.c
@@ -27,6 +27,12 @@ BUILD_ASSERT(((NRF_DRIVE_S0S1 == NRF_GPIO_PIN_S0S1) &&
 	      (1U)),
 	     "nRF pinctrl drive settings do not match HAL values");
 
+#if DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_uart)
+#define NRF_PSEL_UART(reg, line) ((NRF_UART_Type *)reg)->PSEL##line
+#elif DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_uarte)
+#define NRF_PSEL_UART(reg, line) ((NRF_UARTE_Type *)reg)->PSEL.line
+#endif
+
 /**
  * @brief Configure pin settings.
  *
@@ -51,10 +57,32 @@ __unused static void nrf_pin_configure(pinctrl_soc_pin_t pin,
 int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 			   uintptr_t reg)
 {
-	ARG_UNUSED(reg);
-
 	for (uint8_t i = 0U; i < pin_cnt; i++) {
 		switch (NRF_GET_FUN(pins[i])) {
+#if defined(NRF_PSEL_UART)
+		case NRF_FUN_UART_TX:
+			NRF_PSEL_UART(reg, TXD) = NRF_GET_PIN(pins[i]);
+			nrf_gpio_pin_write(NRF_GET_PIN(pins[i]), 1);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_OUTPUT,
+					  NRF_GPIO_PIN_INPUT_DISCONNECT);
+			break;
+		case NRF_FUN_UART_RX:
+			NRF_PSEL_UART(reg, RXD) = NRF_GET_PIN(pins[i]);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_INPUT,
+					  NRF_GPIO_PIN_INPUT_CONNECT);
+			break;
+		case NRF_FUN_UART_RTS:
+			NRF_PSEL_UART(reg, RTS) = NRF_GET_PIN(pins[i]);
+			nrf_gpio_pin_write(NRF_GET_PIN(pins[i]), 1);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_OUTPUT,
+					  NRF_GPIO_PIN_INPUT_DISCONNECT);
+			break;
+		case NRF_FUN_UART_CTS:
+			NRF_PSEL_UART(reg, CTS) = NRF_GET_PIN(pins[i]);
+			nrf_pin_configure(pins[i], NRF_GPIO_PIN_DIR_INPUT,
+					  NRF_GPIO_PIN_INPUT_CONNECT);
+			break;
+#endif /* defined(NRF_PSEL_UART) */
 		default:
 			return -ENOTSUP;
 		}