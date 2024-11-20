thesofproject/zephyr

arch/xtensa: soc/xtensa/intel_adsp: Enable KERNEL_COHERENCE
Implement the kernel "coherence" API on top of the linker
cached/uncached mapping work.

Add Xtensa handling for the stack coherence API.

Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

arch/xtensa/include/kernel_arch_func.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2016 Wind River Systems, Inc.
  * Copyright (c) 2016 Cadence Design Systems, Inc.
+ * Copyright (c) 2020 Intel Corporation
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -37,6 +38,13 @@ static ALWAYS_INLINE void arch_kernel_init(void)
 {
 	_cpu_t *cpu0 = &_kernel.cpus[0];
 
+#ifdef CONFIG_KERNEL_COHERENCE
+	/* Make sure we don't have live data for unexpected cached
+	 * regions due to boot firmware
+	 */
+	xthal_dcache_all_writeback_inv();
+#endif
+
 	cpu0->nested = 0;
 
 	/* The asm2 scheme keeps the kernel pointer in MISC0 for easy
@@ -60,6 +68,60 @@ static inline void arch_switch(void *switch_to, void **switched_from)
 	return xtensa_switch(switch_to, switched_from);
 }
 
+/* FIXME: we don't have a framework for including this from the SoC
+ * layer, so we define it in the arch code here.
+ */
+#ifdef CONFIG_SOC_FAMILY_INTEL_ADSP
+static inline bool arch_mem_coherent(void *ptr)
+{
+    size_t addr = (size_t) ptr;
+
+    return addr >= 0x80000000 && addr < 0xa0000000;
+}
+#endif
+
+static inline void arch_cohere_stacks(struct k_thread *old_thread,
+				      void *old_switch_handle,
+				      struct k_thread *new_thread)
+{
+#ifdef CONFIG_KERNEL_COHERENCE
+	size_t ostack = old_thread->stack_info.start;
+	size_t osz    = old_thread->stack_info.size;
+	size_t osp    = (size_t) old_switch_handle;
+
+	size_t nstack = new_thread->stack_info.start;
+	size_t nsz    = new_thread->stack_info.size;
+	size_t nsp    = (size_t) new_thread->switch_handle;
+
+	xthal_dcache_region_invalidate((void *)nsp, (nstack + nsz) - nsp);
+
+	/* FIXME: dummy initializion threads don't have stack info set
+	 * up and explode the logic above.  Find a way to get this
+	 * test out of the hot paths!
+	 */
+	if (old_thread->base.thread_state & _THREAD_DUMMY) {
+		return;
+	}
+
+	/* In interrupt context, we have a valid frame already from
+	 * the interrupt entry code, but for arch_switch() that hasn't
+	 * happened yet.  It will do the flush itself, we just have to
+	 * calculate the boundary for it.
+	 */
+	if (old_switch_handle != NULL) {
+		xthal_dcache_region_writeback((void *)osp,
+					      (ostack + osz) - osp);
+	} else {
+		/* FIXME: hardcoding EXCSAVE3 is bad, should be
+		 * configurable a-la XTENSA_KERNEL_CPU_PTR_SR.
+		 */
+		u32_t end = ostack + osz;
+
+		__asm__ volatile("wsr.EXCSAVE3 %0" :: "r"(end));
+	}
+#endif
+}
+
 #ifdef __cplusplus
 }
 #endif
@@ -68,6 +130,7 @@ static inline bool arch_is_in_isr(void)
 {
 	return arch_curr_cpu()->nested != 0U;
 }
+
 #endif /* _ASMLANGUAGE */
 
 #endif /* ZEPHYR_ARCH_XTENSA_INCLUDE_KERNEL_ARCH_FUNC_H_ */

include/arch/xtensa/arch.h
@@ -27,7 +27,11 @@
 #include <xtensa/config/core.h>
 #include <arch/common/addr_types.h>
 
+#ifdef CONFIG_KERNEL_COHERENCE
+#define STACK_ALIGN XCHAL_DCACHE_LINESIZE
+#else
 #define STACK_ALIGN 16
+#endif
 
 /* Xtensa GPRs are often designated by two different names */
 #define sys_define_gpr_with_alias(name1, name2) union { u32_t name1, name2; }

gzh-terry/external_zblue

arch/xtensa/include/kernel_arch_func.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2016 Wind River Systems, Inc.
  * Copyright (c) 2016 Cadence Design Systems, Inc.
+ * Copyright (c) 2020 Intel Corporation
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -32,6 +33,13 @@ static ALWAYS_INLINE void arch_kernel_init(void)
 {
 	_cpu_t *cpu0 = &_kernel.cpus[0];
 
+#ifdef CONFIG_KERNEL_COHERENCE
+	/* Make sure we don't have live data for unexpected cached
+	 * regions due to boot firmware
+	 */
+	xthal_dcache_all_writeback_inv();
+#endif
+
 	cpu0->nested = 0;
 
 	/* The asm2 scheme keeps the kernel pointer in MISC0 for easy
@@ -55,6 +63,60 @@ static inline void arch_switch(void *switch_to, void **switched_from)
 	return xtensa_switch(switch_to, switched_from);
 }
 
+/* FIXME: we don't have a framework for including this from the SoC
+ * layer, so we define it in the arch code here.
+ */
+#ifdef CONFIG_SOC_FAMILY_INTEL_ADSP
+static inline bool arch_mem_coherent(void *ptr)
+{
+    size_t addr = (size_t) ptr;
+
+    return addr >= 0x80000000 && addr < 0xa0000000;
+}
+#endif
+
+#ifdef CONFIG_KERNEL_COHERENCE
+static inline void arch_cohere_stacks(struct k_thread *old_thread,
+				      void *old_switch_handle,
+				      struct k_thread *new_thread)
+{
+	size_t ostack = old_thread->stack_info.start;
+	size_t osz    = old_thread->stack_info.size;
+	size_t osp    = (size_t) old_switch_handle;
+
+	size_t nstack = new_thread->stack_info.start;
+	size_t nsz    = new_thread->stack_info.size;
+	size_t nsp    = (size_t) new_thread->switch_handle;
+
+	xthal_dcache_region_invalidate((void *)nsp, (nstack + nsz) - nsp);
+
+	/* FIXME: dummy initializion threads don't have stack info set
+	 * up and explode the logic above.  Find a way to get this
+	 * test out of the hot paths!
+	 */
+	if (old_thread->base.thread_state & _THREAD_DUMMY) {
+		return;
+	}
+
+	/* In interrupt context, we have a valid frame already from
+	 * the interrupt entry code, but for arch_switch() that hasn't
+	 * happened yet.  It will do the flush itself, we just have to
+	 * calculate the boundary for it.
+	 */
+	if (old_switch_handle != NULL) {
+		xthal_dcache_region_writeback((void *)osp,
+					      (ostack + osz) - osp);
+	} else {
+		/* FIXME: hardcoding EXCSAVE3 is bad, should be
+		 * configurable a-la XTENSA_KERNEL_CPU_PTR_SR.
+		 */
+		uint32_t end = ostack + osz;
+
+		__asm__ volatile("wsr.EXCSAVE3 %0" :: "r"(end));
+	}
+}
+#endif
+
 #ifdef __cplusplus
 }
 #endif
@@ -63,6 +125,7 @@ static inline bool arch_is_in_isr(void)
 {
 	return arch_curr_cpu()->nested != 0U;
 }
+
 #endif /* _ASMLANGUAGE */
 
 #endif /* ZEPHYR_ARCH_XTENSA_INCLUDE_KERNEL_ARCH_FUNC_H_ */

include/arch/xtensa/arch.h
@@ -28,7 +28,11 @@
 #include <xtensa/config/core.h>
 #include <arch/common/addr_types.h>
 
+#ifdef CONFIG_KERNEL_COHERENCE
+#define ARCH_STACK_PTR_ALIGN XCHAL_DCACHE_LINESIZE
+#else
 #define ARCH_STACK_PTR_ALIGN 16
+#endif
 
 /* Xtensa GPRs are often designated by two different names */
 #define sys_define_gpr_with_alias(name1, name2) union { uint32_t name1, name2; }