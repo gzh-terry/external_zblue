zephyrproject-rtos/zephyr

Bluetooth: Mesh: Add support prioritized message relay
In our use scenario, the gateway needs to control a large
number of light bulbs in real time, which can exceed 100,
and a large number of light bulbs will return the status to
the user every time the status changes, which will cause a
temporary mesh network storm in the whole network.

We optimized the time of status return, which does not happen
immediately, but will return the status for a random number of
seconds, but during the status return, Each status message may
be relayed by all nodes in the entire network.

If the gateway sends a new control message at this time, the
message obviously needs to be queued, resulting in poor consistency
of the entire network control.

The prioritized relay messages will be send before other lower
priority relay message to ensure that higher priority messages
can be sent in time. When considering the message latency, also
consider the values of BT_MESH_RELAY_RETRANSMIT_COUNT and
BT_MESH_RELAY_RETRANSMIT_INTERVAL.

A higher number of BT_MESH_RELAY_ADV_SETS allows the increase in
the number of buffers while maintaining the latency.

Note: To enable this option, the application layer needs to register
a callback function to determine the priority of each relay message.

If it is not registered, the default priority is the same as the lowest.

Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

subsys/bluetooth/mesh/adv.h
@@ -37,6 +37,7 @@ struct bt_mesh_adv {
 		  tag:4;
 
 	uint8_t      xmit;
+	uint8_t      prio;
 };
 
 struct bt_mesh_buf {
@@ -57,9 +58,11 @@ struct bt_mesh_buf *bt_mesh_buf_ref(struct bt_mesh_buf *buf);
 void bt_mesh_buf_unref(struct bt_mesh_buf *buf);
 
 /* xmit_count: Number of retransmissions, i.e. 0 == 1 transmission */
-struct bt_mesh_buf *bt_mesh_adv_create(enum bt_mesh_adv_type type,
-				       enum bt_mesh_adv_tag tag,
-				       uint8_t xmit, k_timeout_t timeout);
+struct bt_mesh_buf *bt_mesh_adv_main_create(enum bt_mesh_adv_type type,
+					    uint8_t xmit, k_timeout_t timeout);
+
+struct bt_mesh_buf *bt_mesh_adv_relay_create(uint8_t prio, uint8_t xmit);
+struct bt_mesh_buf *bt_mesh_adv_frnd_create(uint8_t xmit, k_timeout_t timeout);
 
 void bt_mesh_adv_send(struct bt_mesh_buf *buf, const struct bt_mesh_send_cb *cb,
 		      void *cb_data);

gzh-terry/external_zblue

Change-Id: Ie0db3fbd1f71a0a2a49b3e7e12cb1500737d2ed1

subsys/bluetooth/mesh/adv.h
@@ -41,15 +41,17 @@ struct bt_mesh_adv {
 		  tag:3;
 
 	uint8_t      xmit;
+	uint8_t      prio;
 };
 
 /* Lookup table for Advertising data types for bt_mesh_adv_type: */
 extern const uint8_t bt_mesh_adv_type[BT_MESH_ADV_TYPES];
 
 /* xmit_count: Number of retransmissions, i.e. 0 == 1 transmission */
-struct net_buf *bt_mesh_adv_create(enum bt_mesh_adv_type type,
-				   enum bt_mesh_adv_tag tag,
-				   uint8_t xmit, k_timeout_t timeout);
+struct net_buf *bt_mesh_adv_main_create(enum bt_mesh_adv_type type,
+					uint8_t xmit, k_timeout_t timeout);
+
+struct net_buf *bt_mesh_adv_relay_create(uint8_t prio, uint8_t xmit);
 
 void bt_mesh_adv_send(struct net_buf *buf, const struct bt_mesh_send_cb *cb,
 		      void *cb_data);